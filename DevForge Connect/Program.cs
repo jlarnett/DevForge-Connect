92cfc54192af6653a939f4191cd3de599429e2e3
mmahoney09110 <mmahoney09110@gmail.com>
2024-09-25 00:00:26 -0500

    Added Chat bot window + Pending Changes
    Added: Chat bot
    Pending Approval: Changed nav bar to be responsive. Tweaked UI in some places
    Need: Still can't connect to FastAPI server

--- DevForge Connect/Program.cs
+++ DevForge Connect/Program.cs
@@ -1,76 +1,20 @@
-using DevForge_Connect.Data;
-using DevForge_Connect.SendGrid;
-using DevForge_Connect.SendGrid.Configuration;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using DevForge_Connect.Entities.Identity;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
-
-var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
-builder.Services.AddCors(options =>
+namespace SignalRChat
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-        builder =>
+    public class Program
+    {
+        public static void Main(string[] args)
         {
-            builder.WithOrigins("http://127.0.0.1:8000");
-            builder.WithHeaders("Access-Control-Allow-Headers");
-            builder.WithHeaders("Access-Control-Allow-Origin");
-        });
-});
+            CreateHostBuilder(args).Build().Run();
+        }
 
-//Send Grid service setup
-builder.Services.Configure<SendGridEmailSenderOptions>(options =>
-{
-    // Gets sendgrid secrets from azure key config / azure key vault. 
-    options.ApiKey = builder.Configuration["SendGrid:ApiKey"]!;
-    options.SenderEmail = builder.Configuration["SendGrid:SenderEmail"]!;
-    options.SenderName = "DevForge_Connect";
-});
-
-builder.Services.AddTransient<IEmailSender, SendGridEmailSender>();
-
-builder.Services.AddHostedService<PythonServerHostedService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-//Enables app cors policy for calling FastAPI.
-app.UseCors(MyAllowSpecificOrigins);
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-app.Run();
--- DevForge Connect/wwwroot/js/LLM_API/api.js
+++ DevForge Connect/wwwroot/js/LLM_API/api.js
@@ -1,7 +1,6 @@
 ﻿$(document).ready(function() {
     $("#TestAPIBtn").click(function(){
-        let message = $("#GemeniMessage").val();
-        let sessionId = $("#SessionId").val();
+        let message = $("#Description").val();
 
         if (message === "" || message === undefined) {
             //Check for empty string before sending to API
@@ -9,24 +8,52 @@
         }
         else {
             //Call the FastAPI Function
-            SendMessageGemini(message, sessionId);
+            SendMessageGemini(message);
         }
     }); 
 });
-function SendMessageGemini(message, sessionId) {
+function SendMessageGemini(message) {
     fetch("http://127.0.0.1:8000/genResponse?text=" + message)
         .then(response => {
-        if (!response.ok) {
-            //When we receive a not ok network response from FastAPI Server
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
+            if (!response.ok) {
+                //When we receive a not ok network response from FastAPI Server
+                var today = new Date();
+                if (today.getMinutes() < 9) { var time = today.getHours() + ":0" + today.getMinutes(); }
+                else { var time = today.getHours() + ":" + today.getMinutes(); }
+
+                var encodedMsg = "<div class='border border-primary rounded-4 m-1 p-1'><p><strong>" + "    Chat Bot" + "</strong> <span class='messageTime'>" + time + "</span><hr></p><p>    " + "    I could not process the message. Please try again." + "</p></div>";
+                var element = document.createElement("div");
+                element.innerHTML = encodedMsg;
+                document.getElementById("messages").appendChild(element);
+
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
         })
         .then(data => {
             //What happens when the response is returned successfully from FastAPI Server
-            $("#GemeniResponse").text(data);
+            var today = new Date();
+            if (today.getMinutes() < 9) { var time = today.getHours() + ":0" + today.getMinutes(); }
+            else { var time = today.getHours() + ":" + today.getMinutes(); }
+
+            var encodedMsg = "<div class='border border-primary rounded-4 m-1 p-1'><p><strong>" + "    Chat Bot" + "</strong> <span class='messageTime'>" + time + "</span><hr></p><p>    " + data + "</p></div>";
+            var element = document.createElement("div");
+            element.innerHTML = encodedMsg;
+            document.getElementById("messages").appendChild(element);
+
         })
-        .catch(error => console.error('Fetch error:', error));
+        .catch(error => {
+            var today = new Date();
+            if (today.getMinutes() < 9) { var time = today.getHours() + ":0" + today.getMinutes(); }
+            else { var time = today.getHours() + ":" + today.getMinutes(); }
+
+            var encodedMsg = "<div class='border border-primary rounded-4 m-1 p-1'><p><strong>" + "    Chat Bot" + "</strong> <span class='messageTime'>" + time + "</span><hr></p><p>    " + "    I could not process the message due to "+ error +". Please try again." + "</p></div>";
+            var element = document.createElement("div");
+            element.innerHTML = encodedMsg;
+            document.getElementById("messages").appendChild(element);
+
+            console.error('Fetch error:', error);
+        })
 }
 
 
