61cd32a318815e7643c6953774e922469e5bd80e
aholmes100 <167231449+aholmes100@users.noreply.github.com>
2024-10-27 14:28:51 -0500

    I pulled from the current master and re-added all of my changes to the user profiles (Auto-create profile, fixed pfp not loading in navbar, and added user expirience).

---
+++ DevForge Connect/Migrations/20241027190140_userExpirience.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevForge_Connect.Migrations
+{
+    /// <inheritdoc />
+    public partial class userExpirience : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Expirience",
+                table: "UserProfile",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Expirience",
+                table: "UserProfile");
+        }
+    }
+}
