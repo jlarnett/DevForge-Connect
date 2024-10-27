fbafd2d14e5ad3d92ae4fc2e2f5744c91202496f
Johnny Arnett <im10g@hotmail.com>
2024-10-27 17:30:21 -0500

    Created a experimental UI for chatbot. Also played with the prompting a good bit trying to get better responses from gemini

--- AI_LLM_Analysis/textGen.py
+++ AI_LLM_Analysis/textGen.py
@@ -9,14 +9,15 @@ genai.configure(api_key=os.environ["GEMENI_API_KEY"])
 
 model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
-    system_instruction="You are a helpful assistant, who is able to identify key requirements in a requirment gathering process. Please keep you're answers as concise as possible.")
+    system_instruction="You are a helpful assistant, who is able to identify key requirements in a requirement gathering process. There are 4 questions you are trying to ask the client. 1 - What is the overall vision of the project? 2 - What are the most important features and requirements? 3 - What technology stacks does your team have skills with or want to use for the project? 4 - What is the deadline of the project? 5 - How much is the client willing to spend on the project? Please keep you're LLM responses concise, and ask the questions one by one. DONT BE SCARED TO RECOMMEND TECHNOLOGIES STACKS THEY MIGHT USE")
 # response = model.generate_content("Write a story about a magic backpack.")
 # print(response.text)
 
 chat = model.start_chat(
     history=[
-        {"role": "user", "parts": "Hello, I'm going to be asking questions about projects and I need help flushing out the requirements. Please keep answers as concise as possible"},
-        {"role": "model", "parts": "Great to meet you. What would you like to know?"},
+        {"role": "user", "parts": "Hello, I'm going to be asking questions about projects and I need help flushing out the requirements. After each response between you and client can you PLEASE give a well formatted SHORT summary of the gather project details. I want the Title, Description, Requirements,deadline, and funding sections, Technologies Required to be included"},
+        {"role": "user", "parts": "I would like you to return a well formatted JSON object back to me with the gathered responses"},
+        {"role": "model", "parts": "Great to meet you! Please give me some details about the vision of your project? What are some of the features you would like to see?"},
     ]
 )
 
