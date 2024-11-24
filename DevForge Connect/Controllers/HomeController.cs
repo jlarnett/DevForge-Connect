1d17e5f01f732d8db0d8e1c32a2b618695aa9a36
Tyler Lofthouse <telofthouse@gmail.com>
2024-11-24 11:33:59 -0600

    start of admin dashboard

--- DevForge Connect/Controllers/HomeController.cs
+++ DevForge Connect/Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace DevForge_Connect.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
+
     }
 }
