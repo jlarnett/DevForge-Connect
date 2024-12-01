032c151526c58ab607cf16758ef80e3aa549f45e
Johnny Arnett <im10g@hotmail.com>
2024-12-01 16:11:10 -0600

    Improved prompts a bit, hopefully concise, but still helpful

--- AI_LLM_Analysis/textGen.py
+++ AI_LLM_Analysis/textGen.py
@@ -12,13 +12,14 @@ genai.configure(api_key=os.getenv("GEMENI_API_KEY"))
 
 model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
-    system_instruction="You are a helpful assistant, who is able to identify key requirements in a requirement gathering process. There are 4 questions you are trying to ask the client. 1 - What is the overall vision of the project? 2 - What are the most important features and requirements? 3 - What technology stacks does your team have skills with or want to use for the project? 4 - What is the deadline of the project? 5 - How much is the client willing to spend on the project? Please keep you're LLM responses concise, and ask the questions one by one. DONT BE SCARED TO RECOMMEND TECHNOLOGIES STACKS THEY MIGHT USE")
+    system_instruction="You are a helpful assistant, who is able to identify key requirements in a requirement gathering process. There are 4 questions you are trying to ask the client 1 - What is the overall vision of the project? 2 - What are the most important features and requirements? 3 - What technology stacks does your team have skills with or want to use for the project? 4 - What is the deadline of the project? 5 - How much is the client willing to spend on the project?. If the user doesn't kow the answer recommend potential answers, but ask the questions 1 per response")
 # response = model.generate_content("Write a story about a magic backpack.")
 # print(response.text)
 
 chat = model.start_chat(
     history=[
-        {"role": "user", "parts": "Hello, I'm going to be asking questions about projects and I need help flushing out the requirements. After each response between you and client can you PLEASE give a well formatted SHORT summary of the gather project details."},
+        {"role": "user", "parts": "Hello, I'm going to be asking questions about projects and I need help flushing out the requirements."},
+        {"role": "user", "parts": "If the user doesn't give you a response recommend a tech stack or possible answers to the question"},
         {"role": "user", "parts": "If I send you 'summarize project' return a json formatted summary. I want the json to include Title, Description, Requirements,deadline, funding, and TechnologiesRequired to be included. The json SHOULD NOT include nested arrays or keys"},
         {"role": "model", "parts": "Great to meet you! Please give me some details about the vision of your project? What are some of the features you would like to see?"},
     ]
